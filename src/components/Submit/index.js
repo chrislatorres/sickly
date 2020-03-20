372f5cab3cf59b0c45bdce974e54fa05a2980050
ltchris <me@chrislatorres.com>
2020-03-19 19:47:51 -0500

    add mapped input for forms

--- src/components/Submit/index.js
+++ src/components/Submit/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import F from 'futil'
+import _ from 'lodash'
 import Form from 'mobx-autoform'
 import { observable, reaction } from 'mobx'
 import { observer } from 'mobx-react'
@@ -9,116 +10,79 @@ import {
   FormFooter,
   FormField,
   DateTimeInput,
+  NestedPicker
   TextInput,
   Button,
   Grid, 
   GridItem, 
   Box 
 } from 'grey-vest'
+import RingLoader from "react-spinners/RingLoader"
 import s from '../../assets/css/page.css'
 import about from '../../assets/images/about.jpg'
+import { Input } from './input.js'
 
-  let form = observable(Form({
-    fields: {
-      email: { value: 'test' },
-      date: new Date('2019-12-31T05:00Z'),
-      source: '',
-      location: '',
-      confirmed: '',
-      deceased: '',
-      recovered: '',
-      comments: '',
-    },
-    submit: async snapshot => {
-  	// Throwing here will capture errors
-      //await serviceCall(snapshot)
-      console.log(snapshot)
-    },
-  }))
+let state = observable({
+  loading: false,
+  sent: false,
+})
 
+let form = observable(Form({
+  fields: {
+    email: { props: { label: 'Email', required: false }, value: '' },
+    date: { props: { label: 'Date', type: 'date', required: true, placeholder: '01/15/2020'}, value: '' },
+    source: { props: { label: 'Source', required: true }, value: '' },
+    location: { props: { label: 'Location', required: true }, value: '' },
+    confirmed: { props: { label: 'Confirmed', type: 'number', required: true }, value: '' },
+    deceased: { props: { label: 'Deceased', type: 'number', required: true }, value: '' },
+    recovered: { props: { label: 'Recovered', type: 'number', required: true }, value: '' },
+    comments: { props: { label: 'Comments', type: 'textarea', width: 2, required: false }, value: '' },
+  },
+  submit: async snapshot => {
+    console.log(snapshot)
+    state.loading = true
+    //await serviceCall(snapshot)
+    console.log(snapshot)
+  },
+}))
 
-const Submit = observer(() => {
-  console.log(form.fields.email.value)
+const Submit = observer(() => 
+  state.loading === true ? 
+    <Box className={s.box}> 
+      <div className={s.container}>
+        <h1>Loading...</h1>  
+        <RingLoader
+          size={150}
+          color={"#0076de"}
+          loading={state.loading}
+          css={"display: inline-block; margin-right: 50px; margin-bottom: 50px"}
+        />
+      </div>
+    </Box>
+    : 
+    state.sent ? <h1>Sent!</h1> :
 
-  let { email, date, source, location, confirmed, deceased, recovered, comments } = form.fields  
-  return (
     <Box className={s.box}> 
       <div className={s.container}>
           <h1>Submit A New Case</h1>
           <p>
-    	 Hi I am <b>Christopher</b>, a freelance Software Engineer out of Dallas, Texas.<br/>
-    	 I created Sickly to help people face the data on COVID-19 cases.<br/>
-    	 Sickly texts your phone when there is a new case near you.<br/>
+            You can submit a new COVID-19 case to Sickly here.<br/>
+    	   <b>NOTE:</b> Please only report cases found from reputable, verifiable sources.<br/>
           </p>
           <div className={s.form}>
              <FormContent columns={2}>
-               <FormField
-                 width={1}
-                 required
-                 {...{
-                   ...F.domLens.value('value', form.fields.email),
-                   ...F.domLens.focus('focusing', form.fields.email),
-                 }}
-                 label="Your Email"
-               />
-               <DateTimeInput 
-                 native 
-                 onChange={e => setState({ ...state, date: e.target.value })} 
-               />
-               <FormField
-                 width={2}
-                 required
-                 
-                 label="Source"
-                 tooltip="A reputable, verifiable source to verify the toll."
-               />
-               <FormField
-                 width={1}
-                 required
-                 value={location} 
-                 onChange={e => setState({ ...state, location: e.target.value })} 
-                 label="Location"
-                 placeholder="Miami, Florida"
-                 tooltip="Location of the case(s)."
-               />
-               <FormField
-                 width={1}
-                 value={confirmed} 
-                 onChange={e => setState({ ...state, confirmed: e.target.value })} 
-                 label="Confirmed"
-                 type="number"
-                 tooltip="Number of confirmed case(s)."
-               />
-               <FormField
-                 width={1}
-                 value={deceased} 
-                 onChange={e => setState({ ...state, deceased: e.target.value })} 
-                 label="Deceased"
-                 type="number"
-                 tooltip="Number of deceased case(s)."
-               />
-               <FormField
-                 width={1}
-                 value={recovered} 
-                 onChange={e => setState({ ...state, recovered: e.target.value })} 
-                 label="Recovered"
-                 type="number"
-                 tooltip="Number of recovered case(s)."
-               />
-               <FormField
-                 width={2}
-                 value={comments} 
-                 onChange={e => setState({ ...state, comments: e.target.value })} 
-                 label="Comments"
-               />
+               {_.map(form.fields, (field, values) =>
+                 <Input
+                   field={field}
+                 />
+               )}
              </FormContent>
              <FormFooter>
-               <Button primary>Submit</Button>
+               <Button primary onClick={form.submit}>Submit</Button>
              </FormFooter>
            </div>
       </div>
     </Box>
-  )
-})
+)
 
 export default () => <Submit />
